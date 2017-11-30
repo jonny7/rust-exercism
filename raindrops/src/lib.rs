3c13bb80aff993cf5e2697a2592bd7e3995afb07
Jonny7 <jonatho.cooper7@gmail.com>
2017-11-30 18:41:24 -0400

    completed raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+
+    // make a new string to concatenate to
+    let mut r = String::new();
+
+    if n % 3 == 0 { r += "Pling" }
+    if n % 5 == 0 { r += "Plang" }
+    if n % 7 == 0 { r += "Plong" }
+
+    // convert n to string
+    if r.is_empty() {
+        r += &n.to_string()
+    }
+    // r is returned
+    r
 }
